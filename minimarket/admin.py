f9be8ec3799b15ac475d42017bc027a69131a30c
YhonVivas <yohnvivas16@gmail.com>
2024-05-26 18:04:39 -0400

    admin

--- minimarket/admin.py
+++ minimarket/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from minimarket.models import Categoria, Producto
+
 # Register your models here.
+
+@admin.register(Producto)
+class ProductosAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Categoria)
+class CategoriasAdmin(admin.ModelAdmin):
+    pass
