a9c97f7312e090c7e80917a925820f342412aecb
Dayana-HR <dayanahoyosriascos@gmail.com>
2024-05-25 21:43:41 -0400

    first commit up project

--- README.md
+++
@@ -1,1 +0,0 @@
-Página web realizada como solución informática a supermercado local. Santiago, Chile.
---
+++ minimarket/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MinimarketConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'minimarket'
