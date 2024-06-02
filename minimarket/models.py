2df3421fbbb170a85c83290f5ed4fe3a2cffb66a
YhonVivas <yohnvivas16@gmail.com>
2024-06-02 13:54:04 -0400

    actualizo models

--- minimarket/models.py
+++ minimarket/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 import uuid
 
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(primary_key=True,max_length=100)
+
 class Producto(models.Model):
     id_producto = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
     nombre = models.CharField(max_length=100)
@@ -9,5 +12,3 @@ class Producto(models.Model):
     imagen = models.ImageField(upload_to='static/img', null=True)
     nombre_categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)  
     
-class Categoria(models.Model):
-    nombre_categoria = models.CharField(primary_key=True,max_length=100)
